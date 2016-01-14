afd3851b7df57ced59be0830c9607dfa4edaa080
Dhruv Agarwal <dhruvagga@gmail.com>
2016-01-15 04:37:14 +0530

    fix required_view method to correctly fetch args

--- flask_restdemo.py
+++ flask_restdemo.py
@@ -9,6 +9,7 @@ class CustomApi(Api):
     def add_resource(self, resource, *urls, **kwargs):
         if kwargs.get('demo_dict'):
             resource = get_demo_resource(resource, kwargs['demo_dict'])
+        kwargs.pop('demo_dict', None)
         super(CustomApi, self).add_resource(resource, *urls, **kwargs)
 
 
@@ -25,11 +26,14 @@ def get_demo_resource(resource, demo_dict):
 
 
 def get_demo_view(demo_responses, default_view=null_view):
-    def required_view(self, *args):
+    def required_view(self, **kwargs):
+        # fetching args in order from kwargs
+        args = [kwargs[x] for x in default_view.func_code.co_varnames[1:]]
+
         for pattern, response in demo_responses.iteritems():
             if ismatch(pattern, args):
                 return response
-        return default_view(args)
+        return default_view(self, **kwargs)
     return required_view
 
 
